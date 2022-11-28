d2ead1544f87ebe2878257aa56379f810dab9024
jingyu9804 <swc05161@gmail.com>
2022-11-28 19:36:42 +0900

    feat(ch7) : super 기능 테스트 구현

--- src/main/java/ch7/PointTest.java
+++ src/main/java/ch7/PointTest.java
@@ -2,13 +2,18 @@ package ch7;
 
 public class PointTest {
     public static void main(String[] args) {
-        Point3D point3D = new Point3D();
+        Point3D point3D = new Point3D(1,2,3);
         System.out.println(point3D.getLocation());
     }
 }
 
 class Point{
-    int x=1,y=2;
+    int x,y;
+
+    Point(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
 
     String getLocation(){
         return "x : "+x+", y : "+y;
@@ -16,7 +21,12 @@ class Point{
 }
 
 class Point3D extends Point{
-    int z=3;
+    int z;
+
+    Point3D(int x,int y,int z){
+        super(x,y);
+        this.z=z;
+    }
 
     String getLocation(){
         return super.getLocation()+", z : "+z;
